62f595e0768794ccf018bab0c1e8836e11bf5970
Ilian002-0 <ilianrogueone@gmail.com>
2025-10-22 18:35:29 +0200

    feat(header): Remove unused SyncIcon component
    The SyncIcon component was present in the Header component but was not being used. This commit removes the component to simplify the codebase.

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,44 +12,18 @@ interface HeaderProps {
 }
 
 const SyncIcon: React.FC<{ isSyncing?: boolean }> = ({ isSyncing }) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`h-5 w-5 ${isSyncing ? 'animate-spin' : ''}`}
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2.5}
-    >
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4 4v5h5"
-        />
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M20 20v-5h-5"
-        />
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4 9a9 9 0 0114.18-5.31"
-        />
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M20 15a9 9 0 01-14.18 5.31"
-        />
-    </svg>
-);
-
-
-const ShareIcon: React.FC = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M3 11.9998C3 7.02919 7.02944 2.99976 12 2.99976C14.8273 2.99976 17.35 4.30342 19 6.34242" />
     <path d="M19.5 2.99976L19.5 6.99976L15.5 6.99976" />
     <path d="M21 11.9998C21 16.9703 16.9706 20.9998 12 20.9998C9.17273 20.9998 6.64996 19.6961 5 17.6571" />
     <path d="M4.5 20.9998L4.5 16.9998L8.5 16.9998" />
+    </svg>
+);
+
+
+const ShareIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
     </svg>
 )
 
